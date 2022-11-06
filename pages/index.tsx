611f8cdd894f1d99984f664ce763ef0dfff6503c
luojiyin <luojiyin@hotmail.com>
2022-11-06 17:15:37 +0800

    feat: select item

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,14 +25,51 @@ export default function Home() {
       { value: "q1strong_careless", text: "我不在乎" },
       {
         value: "q1strong_1",
-        info: "只选择流行并广泛使用或带有强烈社区支持的许可协议",
+        text: "只选择流行并广泛使用或带有强烈社区支持的许可协议",
       },
       { value: "q1strong_0", text: "选择所有许可协议" },
+      
     ],
     [
       { value: "q2anocopyleft_0", text: "我想对代码的重复使用设置许可条件" },
       { value: "q2anocopyleft_1", text: "我不想对代码的重复使用设置许可条件" },
     ],
+    [
+
+      {value:"q2bstrong_1", text:"强著佐权 （Copyleft）"},
+      {value:"q2bweak_1", text:"弱著佐权 （Copyleft）"}
+    ],
+    [
+      {value:"q2cmod_1", text:"模块级别"},
+      {value:"q2cfile_1", text:"文件级别"},
+      {Value:"q2clib_1", text:"库接口级别"}
+    ],
+    [
+      {value:"q3juris_careless",text:"我不在乎——即使指定了境外地址"},
+      {value:"q3juris_0", text:"我想使用未涉及该话题的许可协议"},
+      {value:"q3juris_1", text:"我想将自己所在区域作为司法管辖区"}
+    ],
+    [
+      {value:"q4apat_careless", text:"我不在乎——我想自己并不拥有专利，即使有也不在乎怎样处理它们"},
+      {value:"q4apat_0", text:"我想使用未涉及专利授权的许可协议，虽然它可能已授权专利"},
+      {value:"q4apat_1", text:"我想使用明确授予专利权的许可协议（如果有）"}
+    ],
+    [
+      {value:"q4bpatret_careless", text:"我不在乎——请提供全部许可协议"},
+      {value:"q4bpatret_1", text:"我想使用包含专利报复条款的许可协议"},
+      {value:"q4bpatret_0", text:"我想使用未包含专利报复条款的许可协议"}
+    ],
+    [
+      {value:"q6noloophole_careless", text:"我不在乎——请提供全部许可协议"},
+      {value:"q6noloophole_1", text:"我想使用解决“隐私漏洞”的许可协议"},
+      {value:"q6noloophole_0", text:"我想使用未解决“隐私漏洞”的许可协议"}
+    ],
+    [
+      {value:"q7nopromo_careless", text:"我不在乎——请提供全部许可协议"},
+      {value:"q7nopromo_1",text:"我想使用禁止推广的许可协议"},
+      {value:"q7nopromo_0", text:"我想使用未禁止推广的许可协议"}
+    ]
+
   ];
 
   const handleSelect = (e) => {
